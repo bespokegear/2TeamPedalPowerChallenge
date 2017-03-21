619c0a6da6deaf5e196477aaf4432c4efcb3f1c7
Matthew Gates <matthewg42@gmail.com>
2017-03-21 14:13:25 +0000

    actually send display / add countdown

--- firmware/Display.cpp
+++ firmware/Display.cpp
@@ -2,6 +2,8 @@
 #include <MutilaDebug.h>
 #include "Display.h"
 
+char buf[13];
+
 Display_ Display;
 
 void Display_::begin()
@@ -17,17 +19,32 @@ void Display_::countdown(uint8_t n)
     } else {
         DBLN(F("DISPLAY [countdown]: GO!"));
     }
+    memset(buf,0,13);   
+    snprintf(buf, 12, "aAACD%d------", n);
+    Serial.println(buf);
 }
 
 void Display_::say(const char* str)
 {
-    DB(F("DISPLAY [string]: "));
+    DB(F("DISPLAY [str - TODO]: "));
     DBLN(str);
+}
+
+void Display_::countdown(uint16_t tenthsSec)
+{
+    DB(F("DISPLAY [countdown]: "));
+    DBLN(tenthsSec);
+    memset(buf,0,13);   
+    snprintf(buf, 12, "aAACD%04d---", tenthsSec);
+    Serial.println(buf);
 }
 
 void Display_::winner(uint8_t n)
 {
     DB(F("DISPLAY [winner]: "));
     DBLN(n);
+    memset(buf,0,13);   
+    snprintf(buf, 12, "aAAWN%d------", n);
+    Serial.println(buf);
 }
 
--- firmware/Display.h
+++ firmware/Display.h
@@ -13,6 +13,8 @@ public:
     void countdown(uint8_t n);
     //! with NULL terminated string
     void say(const char* str);
+    //! countdown in tenths of a sec (e.g. to display "30", tenthsSec=300)
+    void countdown(uint16_t tenthsSec);
     //! say who won. if n == 0 it means a tie
     void winner(uint8_t n);
 
