ba02b87a4abbc185ac2b9e4e01a243646e945b5f
Matthew Gates <matthewg42@gmail.com>
2017-03-21 11:40:14 +0000

    matrix display test

---
+++ matrix_display_test/matrix_display_test.ino
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <stdio.h>
+
+char buf[14];
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(300);
+}
+
+void loop()
+{
+    Serial.println(F("aAACD3------")); delay(1000);
+    Serial.println(F("aAACD2------")); delay(1000);
+    Serial.println(F("aAACD1------")); delay(1000);
+    Serial.println(F("aAACD0------")); delay(1000);
+    for (int i=300; i>0; i--) {
+        memset(buf, 0, 14);
+        snprintf(buf, 13, "aAAT%04d----", i);
+        Serial.println(buf);
+        delay(100);
+    }
+    Serial.println(F("aAAWN4------"));
+    delay(3000);
+}
