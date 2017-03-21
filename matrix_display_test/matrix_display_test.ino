289aaa59bd09628ac3710fc920cfb5c18f03d6dd
Matthew Gates <matthewg42@gmail.com>
2017-03-21 16:10:21 +0000

    take out debugging to make matrix work

--- matrix_display_test/matrix_display_test.ino
+++ matrix_display_test/matrix_display_test.ino
@@ -15,12 +15,12 @@ void loop()
     Serial.println(F("aAACD2------")); delay(1000);
     Serial.println(F("aAACD1------")); delay(1000);
     Serial.println(F("aAACD0------")); delay(1000);
-    for (int i=300; i>0; i--) {
+    for (int i=50; i>0; i--) {
         memset(buf, 0, 14);
         snprintf(buf, 13, "aAAT%04d----", i);
         Serial.println(buf);
         delay(100);
     }
-    Serial.println(F("aAAWN4------"));
+    Serial.println(F("aAAWN2------"));
     delay(3000);
 }
