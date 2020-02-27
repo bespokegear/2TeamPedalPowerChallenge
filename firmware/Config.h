72e1f7d63eb1598963d0764741288a4407bc8702
Matthew Little <matt@re-innovation.co.uk>
2020-02-27 18:35:18 +0000

    Update Config.h

--- firmware/Config.h
+++ firmware/Config.h
@@ -59,5 +59,5 @@
 
 #define COUNTER_ONSET_DELAY_MS      1000
 
-#define LED_TIMER_DELAY_MS          5000            // Timer mS - how long to hold the LEDs on after voltage dropped OR victory display
+#define LED_TIMER_DELAY_MS          30000            // Timer mS - how long to hold the LEDs on after voltage dropped OR victory display
 #define COUNTDOWN_COLOUR            0xFFFFFFFF      // Colour of countdown LEDS
